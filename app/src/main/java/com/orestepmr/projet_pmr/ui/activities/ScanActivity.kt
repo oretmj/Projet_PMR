a8f9a68f9439e28200e1b43e6621bed565e5c709
Firemain <87fugiii@gmail.com>
2024-06-20 12:52:45 +0200

    update Scanacitivy, manque l'implémentation du coffre 3D

--- app/src/main/java/com/orestepmr/projet_pmr/ui/activities/ScanActivity.kt
+++ app/src/main/java/com/orestepmr/projet_pmr/ui/activities/ScanActivity.kt
@@ -9,11 +9,26 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.ar.core.Anchor
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.ArSceneView
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.MaterialFactory
+import com.google.ar.sceneform.rendering.ShapeFactory
 import com.google.zxing.integration.android.IntentIntegrator
 import com.journeyapps.barcodescanner.CaptureActivity
 import com.orestepmr.projet_pmr.R
+import com.google.ar.sceneform.ux.TransformationSystem
+import com.google.ar.sceneform.ux.TransformableNode
+import com.google.ar.sceneform.ux.FootprintSelectionVisualizer
 
 class ScanActivity : CaptureActivity() {
+
+    private val VOICE_RECOGNITION_REQUEST_CODE = 1234
+    private lateinit var arSceneView: ArSceneView
+    private lateinit var transformationSystem: TransformationSystem
+
+
 
     companion object {
         private const val REQUEST_CAMERA = 1
@@ -40,10 +55,17 @@ class ScanActivity : CaptureActivity() {
     private fun setupScanner() {
         setContentView(R.layout.activity_scan)
 
+
+        //AR
+        arSceneView = findViewById(R.id.ar_scene_view)
+        val selectionVisualizer = FootprintSelectionVisualizer()
+        transformationSystem = TransformationSystem(resources.displayMetrics, selectionVisualizer)
+
+
+
         findViewById<Button>(R.id.btn_new_scan).setOnClickListener {
             startQRScanner()
         }
-        // Ajoute d'autres configurations si nécessaire
     }
     private fun startQRScanner() {
         // Configuration et lancement du scanner ZXing
@@ -70,15 +92,48 @@ class ScanActivity : CaptureActivity() {
                     .create()
                     .show()
             } else {
-                // Display the result in a dialog or use the result in other ways
-                AlertDialog.Builder(this)
-                    .setTitle("Scan Result")
-                    .setMessage("QR Code content:\n${result.contents}")
-                    .setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
-                    .create()
-                    .show()
+
+                // Display 3D object
+                display3DChest(result.contents)
+
             }
         }
     }
+
+    private fun display3DChest(contents: String) {
+        //Gestion de la 3D pour afficher un coffre
+
+        val anchor = arSceneView.session?.createAnchor(arSceneView.arFrame?.camera?.pose)
+        if (anchor != null) {
+            placeObject(arSceneView, anchor)
+        }
+
+        // Mettre un bouton invisible pour dire "ouvrir" avec la bibliothèque realwear
+        //openChest(contents)
+    }
+
+    private fun placeObject(arSceneView: ArSceneView, anchor: Anchor) {
+        MaterialFactory.makeOpaqueWithColor(this, com.google.ar.sceneform.rendering.Color(android.graphics.Color.WHITE))
+            .thenAccept { material ->
+                val modelRenderable = ShapeFactory.makeCube(Vector3(0.1f, 0.1f, 0.1f), Vector3.zero(), material)
+                val anchorNode = AnchorNode(anchor)
+                anchorNode.setParent(arSceneView.scene)
+                val node = TransformableNode(transformationSystem)
+                node.renderable = modelRenderable
+                node.setParent(anchorNode)
+                node.select()
+            }
+    }
+
+    private fun openChest(contents: String) {
+        //Open the right enigma with the contents value of the QR code
+        AlertDialog.Builder(this)
+                    .setTitle("Scan Result")
+                    .setMessage("QR Code content:\n${contents}")
+                    .setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
+                    .create()
+                    .show()
+    }
+
 
 }
